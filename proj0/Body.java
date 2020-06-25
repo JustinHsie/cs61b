c5b7e54026404deb6b4c32361ea7234ea27af22e
Justin Hsie <justinhsie1001@gmail.com>
2020-06-25 12:33:09 -0700

    Added methods for calcDistance and calcForceExertedBy

--- proj0/Body.java
+++ proj0/Body.java
@@ -1,6 +1,10 @@
+/** Import packages */
+import java.lang.Math;
+
 /** Class that determines properties of the planet body
 * @author Justin Hsie
 */
+
 public class Body {
 	public double xxPos;
 	public double yyPos;
@@ -8,6 +12,11 @@ public class Body {
 	public double yyVel;
 	public double mass;
 	public String imgFileName;
+	public double dx;
+	public double dy;
+	public double r;
+	public double netForce;
+	public static final double constG = 6.67e-11;
 
 	/** Constructor instantiate class */
 	public Body(double xP, double yP, double xV,
@@ -29,4 +38,30 @@ public class Body {
 		this.mass = b.mass;
 		this.imgFileName = b.imgFileName;
 	}
+
+	/** Method to calculate distance between 
+	this body and given body */
+	public double calcDistance(Body b) {
+		dx = this.xxPos - b.xxPos;
+		dy = this.yyPos - b.yyPos;
+		r = Math.sqrt(dx * dx + dy * dy);
+		return r;
+	}
+
+	/** Method to calculate force exerted on 
+	this body by given body */
+	public double calcForceExertedBy(Body b) {
+		netForce = (constG * this.mass * b.mass) / 
+					(calcDistance(b) * calcDistance(b));
+		return netForce;
+	}
 }
+
+
+
+
+
+
+
+
+
