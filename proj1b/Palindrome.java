bbaf0f4271bd8e364cd14569ea289493817c43ec
Justin Hsie <justinhsie1001@gmail.com>
2020-07-11 21:26:38 -0700

    Implemented wordToDeque method

--- proj1b/Palindrome.java
+++ proj1b/Palindrome.java
@@ -1,5 +1,9 @@
 public class Palindrome<Item> {
     public Deque<Character> wordToDeque(String word) {
-        return null;
+        Deque d = new LinkedListDeque();
+        for (int i = 0; i < word.length(); i++) {
+            d.addLast(word.charAt(i));
+        }
+        return d;
     }
 }
