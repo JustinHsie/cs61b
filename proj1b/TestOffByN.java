16bb3f0e0b03c67940071b3d0a804a71a3676b1c
Justin Hsie <justinhsie1001@gmail.com>
2020-07-12 21:23:31 -0700

    Created tests for OffByN

--- proj1b/TestOffByN.java
+++ proj1b/TestOffByN.java
@@ -1,2 +1,13 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class TestOffByN {
+
+    static CharacterComparator offByN = new OffByN(3);
+
+    @Test
+    public void testEqualChars() {
+        assertTrue(offByN.equalChars('a', 'd'));
+        assertFalse(offByN.equalChars('f', 'z'));
+    }
 }
