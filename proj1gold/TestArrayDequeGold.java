c175d8c7f507575a56cde580532f61c540243e38
Justin Hsie <justinhsie1001@gmail.com>
2020-07-13 20:43:11 -0700

    Cleaned up message concat to +=

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -11,7 +11,7 @@ public class TestArrayDequeGold {
          */
 
         StudentArrayDeque<Integer> sad1 = new StudentArrayDeque<>();
-        String message = "";
+        String message = "\n";
 
         for (int i = 0; i < 500; i += 1) {
             double numberBetweenZeroAndOne = StdRandom.uniform(6);
@@ -21,33 +21,33 @@ public class TestArrayDequeGold {
             if (numberBetweenZeroAndOne < 1) {
                 sad1.addLast(i);
                 ads1.addLast(i);
-                message = message.concat("addLast(" + i + ")\n");
+                message += "addLast(" + i + ")\n";
             } else if (numberBetweenZeroAndOne == 1){
                 sad1.addFirst(i);
                 ads1.addFirst(i);
-                message = message.concat("addFirst(" + i + ")\n");
+                message += "addFirst(" + i + ")\n";
             } else if (ads1.size() == 0) {
                 i -= 1;
                 continue;
             } else if (numberBetweenZeroAndOne == 2) {
                 actual = sad1.removeLast();
                 expected = ads1.removeLast();
-                message = message.concat("removeLast()\n");
+                message += "removeLast()\n";
                 assertEquals(message, expected, actual);
             } else if (numberBetweenZeroAndOne == 3) {
                 actual = sad1.removeFirst();
                 expected = ads1.removeFirst();
-                message = message.concat("removeFirst()\n");
+                message += "removeFirst()\n";
                 assertEquals(message, expected, actual);
             } else if (numberBetweenZeroAndOne == 4) {
                 actual = sad1.get(i % ads1.size());
                 expected = ads1.get(i % ads1.size());
-                message = message.concat("get(" + i % ads1.size() + ")\n");
+                message += "get(" + i % ads1.size() + ")\n";
                 assertEquals(message, expected, actual);
             } else if (numberBetweenZeroAndOne == 5) {
                 actual = sad1.size();
                 expected = ads1.size();
-                message = message.concat("size()\n");
+                message += "size()\n";
                 assertEquals(message, expected, actual);
             }
         }
