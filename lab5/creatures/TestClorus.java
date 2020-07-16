f54fdb7ffb5aff9c8f39e69a546d2e6dba5462dd
Justin Hsie <justinhsie1001@gmail.com>
2020-07-15 19:32:21 -0700

    Fixed testGainEnergy test

--- lab5/creatures/TestClorus.java
+++ lab5/creatures/TestClorus.java
@@ -44,12 +44,12 @@ public class TestClorus {
         Plip p = new Plip(2);
         HashMap<Direction, Occupant> eatPlip = new HashMap<Direction, Occupant>();
         eatPlip.put(Direction.TOP, p);
-        eatPlip.put(Direction.BOTTOM, new Impassible());
+        eatPlip.put(Direction.BOTTOM, new Empty());
         eatPlip.put(Direction.LEFT, new Impassible());
         eatPlip.put(Direction.RIGHT, new Impassible());
 
         actual = c.chooseAction(eatPlip);
-        expected = new Action(Action.ActionType.ATTACK);
+        expected = new Action(Action.ActionType.ATTACK, Direction.TOP);
 
         assertEquals(expected, actual);
 
@@ -83,15 +83,15 @@ public class TestClorus {
         c = new Clorus(.99);
 
         actual = c.chooseAction(topEmpty);
-        expected = new Action(Action.ActionType.MOVE);
+        expected = new Action(Action.ActionType.MOVE, Direction.TOP);
         assertEquals(expected, actual);
 
         // Energy < 1, move to empty space
         c = new Clorus(.99);
 
         actual = c.chooseAction(allEmpty);
-        expected = new Action(Action.ActionType.MOVE);
-        assertEquals(expected, actual);
+        unexpected = new Action(Action.ActionType.STAY);
+        assertNotEquals(unexpected, actual);
     }
 
     @Test
@@ -100,13 +100,8 @@ public class TestClorus {
         Plip p = new Plip(2);
         double oldE = c.energy();
         double pipE = p.energy();
-        HashMap<Direction, Occupant> eatPlip = new HashMap<Direction, Occupant>();
-        eatPlip.put(Direction.TOP, p);
-        eatPlip.put(Direction.BOTTOM, new Impassible());
-        eatPlip.put(Direction.LEFT, new Impassible());
-        eatPlip.put(Direction.RIGHT, new Impassible());
 
-        c.chooseAction(eatPlip);
+        c.attack(p);
         assertEquals(oldE + pipE, c.energy(), 0.01);
     }
 }
