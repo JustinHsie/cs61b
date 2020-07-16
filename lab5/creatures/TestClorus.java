10c2b37643a5c1c244af7ca348851b2b6be40ab9
Justin Hsie <justinhsie1001@gmail.com>
2020-07-15 18:22:05 -0700

    Wrote testChoose

--- lab5/creatures/TestClorus.java
+++ lab5/creatures/TestClorus.java
@@ -24,6 +24,77 @@ public class TestClorus {
     }
 
     @Test
+    public void testChoose() {
+
+        // No adjacent empty squares, stay
+        Clorus c = new Clorus(2);
+        HashMap<Direction, Occupant> surrounded = new HashMap<Direction, Occupant>();
+        surrounded.put(Direction.TOP, new Impassible());
+        surrounded.put(Direction.BOTTOM, new Impassible());
+        surrounded.put(Direction.LEFT, new Impassible());
+        surrounded.put(Direction.RIGHT, new Impassible());
+
+        Action actual = c.chooseAction(surrounded);
+        Action expected = new Action(Action.ActionType.STAY);
+
+        assertEquals(expected, actual);
+
+        // Attack plip
+        c = new Clorus(2);
+        Plip p = new Plip(2);
+        HashMap<Direction, Occupant> eatPlip = new HashMap<Direction, Occupant>();
+        eatPlip.put(Direction.TOP, p);
+        eatPlip.put(Direction.BOTTOM, new Impassible());
+        eatPlip.put(Direction.LEFT, new Impassible());
+        eatPlip.put(Direction.RIGHT, new Impassible());
+
+        actual = c.chooseAction(eatPlip);
+        expected = new Action(Action.ActionType.ATTACK);
+
+        assertEquals(expected, actual);
+
+        // Energy >= 1, replicate to empty space
+        c = new Clorus(2);
+        HashMap<Direction, Occupant> topEmpty = new HashMap<Direction, Occupant>();
+        topEmpty.put(Direction.TOP, new Empty());
+        topEmpty.put(Direction.BOTTOM, new Impassible());
+        topEmpty.put(Direction.LEFT, new Impassible());
+        topEmpty.put(Direction.RIGHT, new Impassible());
+
+        actual = c.chooseAction(topEmpty);
+        expected = new Action(Action.ActionType.REPLICATE, Direction.TOP);
+
+        assertEquals(expected, actual);
+
+        // Energy >= 1, replicate to empty space
+        c = new Clorus(2);
+        HashMap<Direction, Occupant> allEmpty = new HashMap<Direction, Occupant>();
+        allEmpty.put(Direction.TOP, new Empty());
+        allEmpty.put(Direction.BOTTOM, new Empty());
+        allEmpty.put(Direction.LEFT, new Empty());
+        allEmpty.put(Direction.RIGHT, new Empty());
+
+        actual = c.chooseAction(allEmpty);
+        Action unexpected = new Action(Action.ActionType.STAY);
+
+        assertNotEquals(unexpected, actual);
+
+        // Energy < 1, move to empty space
+        c = new Clorus(.99);
+
+        actual = c.chooseAction(topEmpty);
+        expected = new Action(Action.ActionType.MOVE);
+        assertEquals(expected, actual);
+
+        // Energy < 1, move to empty space
+        c = new Clorus(.99);
+
+        actual = c.chooseAction(allEmpty);
+        expected = new Action(Action.ActionType.MOVE);
+        assertEquals(expected, actual);
+    }
+
+    @Test
     public void testGainEnergy() {
         Clorus c = new Clorus(2);
         Plip p = new Plip(2);
@@ -38,7 +109,4 @@ public class TestClorus {
         c.chooseAction(eatPlip);
         assertEquals(oldE + pipE, c.energy(), 0.01);
     }
-
-
-
 }
