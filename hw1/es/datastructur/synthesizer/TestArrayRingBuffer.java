89897c9be81ab1359ca87588e87e10c5b6e997f5
Justin Hsie <justinhsie1001@gmail.com>
2020-07-17 14:33:16 -0700

    Added tests

--- hw1/es/datastructur/synthesizer/TestArrayRingBuffer.java
+++ hw1/es/datastructur/synthesizer/TestArrayRingBuffer.java
@@ -9,6 +9,20 @@ import static org.junit.Assert.*;
 public class TestArrayRingBuffer {
     @Test
     public void someTest() {
-        //ArrayRingBuffer arb = new ArrayRingBuffer(10);
+        ArrayRingBuffer arb = new ArrayRingBuffer(4);
+        assertTrue(arb.isEmpty());
+
+        arb.enqueue(9.3);
+        arb.enqueue(15.1);
+        arb.enqueue(31.2);
+        assertFalse(arb.isFull());
+
+        arb.enqueue(-3.1);
+        assertTrue(arb.isFull());
+
+        assertEquals(9.3, arb.dequeue());
+        assertEquals(15.1, arb.peek());
+
+
     }
 }
