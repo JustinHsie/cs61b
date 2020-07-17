a95babc3eb41de3c915cb5b5774a2c0cfad014fd
Justin Hsie <justinhsie1001@gmail.com>
2020-07-17 12:16:24 -0700

    Created BoundedQueue interface

---
+++ hw1/es/datastructur/synthesizer/BoundedQueue.java
@@ -0,0 +1,48 @@
+package es.datastructur.synthesizer;
+
+public interface BoundedQueue<T> {
+
+    /**
+     * Returns size of buffer
+     */
+    int capacity();
+
+    /**
+     * Returns number of items currently in buffer
+     */
+    int fillCount();
+
+    /**
+     * Add item x to the end
+     */
+    void enqueue(T x);
+
+    /**
+     * Delete and return item from the front
+     */
+    T dequeue();
+    /**
+     * Return (but do not delete) item from the front
+     */
+    T peek();
+
+    /**
+     * Is the buffer empty? (fillCount equals zero)
+     */
+    default boolean isEmpty() {
+        if (fillCount() == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Is the buffer full? (fillCount is same as capacity)
+     */
+    default boolean isFull() {
+        if (fillCount() == capacity()) {
+            return true;
+        }
+        return false;
+    }
+}
