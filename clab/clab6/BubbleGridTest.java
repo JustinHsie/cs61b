61fdaca279c2ecc26d1492a50f8a48e689773141
Justin Hsie <justinhsie1001@gmail.com>
2020-07-21 12:22:01 -0700

    Passed tests

--- clab/clab6/BubbleGridTest.java
+++ clab/clab6/BubbleGridTest.java
@@ -8,8 +8,14 @@ public class BubbleGridTest {
 
         int[][] grid = {{1, 0, 0, 0},
                         {1, 1, 1, 0}};
+        int[][] grid2 = {{1, 1, 0},
+                         {1, 0, 0},
+                         {1, 1, 0},
+                         {1, 1, 1}};
         int[][] darts = {{1, 0}};
+        int[][] darts2 = {{2, 2}, {2, 0}};
         int[] expected = {2};
+        int[] expected2 = {0, 4};
 
         validate(grid, darts, expected);
     }
