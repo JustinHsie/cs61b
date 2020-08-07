bc7b12b680fb0b0b27a04ef4b43e2724b2492435
Justin Hsie <justinhsie1001@gmail.com>
2020-08-06 17:11:21 -0700

    Created MyTrieSet class

---
+++ lab9/MyTrieSet.java
@@ -0,0 +1,31 @@
+import java.util.List;
+
+public class MyTrieSet implements TrieSet61B {
+    /** Clears all items out of Trie */
+    public void clear() {
+        //TODO
+    }
+
+    /** Returns true if the Trie contains KEY, false otherwise */
+    public boolean contains(String key) {
+        //TODO
+        return false;
+    }
+
+    /** Inserts string KEY into Trie */
+    public void add(String key) {}
+
+    /** Returns a list of all words that start with PREFIX */
+    public List<String> keysWithPrefix(String prefix) {
+        //TODO
+        return null;
+    }
+
+    /** Returns the longest prefix of KEY that exists in the Trie
+     * Not required for Lab 9. If you don't implement this, throw an
+     * UnsupportedOperationException.
+     */
+    public String longestPrefixOf(String key) {
+        throw new UnsupportedOperationException();
+    }
+}
