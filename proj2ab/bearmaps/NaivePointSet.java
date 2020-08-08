124e740873619477ac1589609282ba05e79ddd9b
Justin Hsie <justinhsie1001@gmail.com>
2020-08-08 15:45:19 -0700

    Implemented NaivePointSet class

---
+++ proj2ab/bearmaps/NaivePointSet.java
@@ -0,0 +1,39 @@
+package bearmaps;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NaivePointSet implements PointSet {
+    private List<Point> list;
+
+    public NaivePointSet(List<Point> points) {
+        list = new ArrayList<>(points.size());
+        list.addAll(points);
+    }
+    public Point nearest(double x, double y) {
+        Point o = new Point(x, y);
+        double bestDist = Double.POSITIVE_INFINITY;
+        Point bestPoint = null;
+
+        for (Point point : list) {
+            double dist = Math.sqrt(Point.distance(point, o));
+            if (dist < bestDist) {
+                bestDist = dist;
+                bestPoint = point;
+            }
+        }
+        return bestPoint;
+    }
+
+
+    public static void main(String[] args) {
+        Point p1 = new Point(1.1, 2.2); // constructs a Point with x = 1.1, y = 2.2
+        Point p2 = new Point(3.3, 4.4);
+        Point p3 = new Point(-2.9, 4.2);
+
+        NaivePointSet nn = new NaivePointSet(List.of(p1, p2, p3));
+        Point ret = nn.nearest(3.0, 4.0); // returns p2
+        System.out.println(ret.getX()); // evaluates to 3.3
+        System.out.println(ret.getY()); // evaluates to 4.4
+    }
+}
