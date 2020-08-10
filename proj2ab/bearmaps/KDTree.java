279cf8ae08e382e0567c4e5d3df0f5f2d2767542
Justin Hsie <justinhsie1001@gmail.com>
2020-08-09 23:49:51 -0700

    Implemented tree constructor

---
+++ proj2ab/bearmaps/KDTree.java
@@ -0,0 +1,59 @@
+package bearmaps;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.ArrayList;
+
+public class KDTree {
+    private HashSet<Point> points;
+    private Node root;
+    int depth;
+
+    public KDTree(List<Point> points) {
+        this.points = new HashSet<>(points);
+        for (Point xy : points) {
+            depth = 0;
+            root = put(root, xy);
+        }
+    }
+
+    private Node put(Node node, Point xy) {
+        if (node == null) {
+            return new Node(xy);
+        }
+
+        int cmp;
+        if (depth % 2 == 0) {
+            cmp = Double.compare(xy.getX(), node.xy.getX());
+        }
+        else {
+            cmp = Double.compare(xy.getY(), node.xy.getY());
+        }
+
+        depth++;
+        if (cmp < 0) {
+            node.left = put(node.left, xy);
+        }
+        else {
+            node.right = put(node.right, xy);
+        }
+        return node;
+    }
+
+    private class Node {
+        Point xy;
+        Node left, right;
+
+        public Node(Point p) {
+            xy = p;
+        }
+    }
+
+    public Point nearest(double x, double y) {
+        return null;
+    }
+
+    private Node nearest(Node n, Point goal, Node best) {
+        return null;
+    }
+}
--- proj2ab/bearmaps/KDTreeTest.java
+++ proj2ab/bearmaps/KDTreeTest.java
@@ -2,6 +2,18 @@ package bearmaps;
 
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import java.util.List;
 
 public class KDTreeTest {
+    @Test
+    public void testPut() {
+        Point a = new Point(2, 3);
+        Point b = new Point(4, 2);
+        Point c = new Point(4, 5);
+        Point d = new Point(3, 3);
+        Point e = new Point(1, 5);
+        Point f = new Point(4, 4);
+
+        KDTree tree = new KDTree(List.of(a, b, c, d, e, f));
+    }
 }
