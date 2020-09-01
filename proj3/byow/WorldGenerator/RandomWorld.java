9a5ac02642caeb5c6ae12348d193e009613d11d1
Justin Hsie <justinhsie1001@gmail.com>
2020-09-01 12:42:22 -0700

    Implemented random neighbor generator

--- proj3/byow/WorldGenerator/RandomWorld.java
+++ proj3/byow/WorldGenerator/RandomWorld.java
@@ -21,11 +21,20 @@ public class RandomWorld {
         //Room room = generateRoom(tiles);
         Room room = new Room(5, 6, new Position(50, 20));
         Draw.drawRoom(room, tiles);
-        generateNeighbor(tiles, room.getBottomLeftCorner(), room.getTopRightCorner());
+        Room room2 = generateNeighbor(tiles, room.getBottomLeftCorner(), room.getTopRightCorner());
+        Room room3 = generateNeighbor(tiles, room2.getBottomLeftCorner(), room2.getTopRightCorner());
+        Room room4 = generateNeighbor(tiles, room3.getBottomLeftCorner(), room3.getTopRightCorner());
 
 
     }
 
+    /**
+     * Takes a room's corners and generates neighbor next to it
+     * @param tiles
+     * @param bottomLeft
+     * @param topRight
+     * @return
+     */
     private static Room generateNeighbor(TETile[][] tiles, Position bottomLeft, Position topRight) {
         Room room;
         Position bottomLeftPosition;
@@ -36,14 +45,50 @@ public class RandomWorld {
             // Min width and height of 3 tiles
             width = RANDOM.nextInt(WIDTH - 3) + 3;
             height = RANDOM.nextInt(HEIGHT - 3) + 3;
-            bottomLeftPosition = new Position(bottomLeft.getX() - width,
-                    RANDOM.nextInt(topRight.getY() - bottomLeft.getY()) + bottomLeft.getY());
+            bottomLeftPosition = randomSide(bottomLeft, topRight, width, height);
         }
         while (outOfBounds(bottomLeftPosition, width, height));
 
         room = new Room(width, height, bottomLeftPosition);
         Draw.drawRoom(room, tiles);
         return room;
+    }
+
+    private static Position randomSide(Position bottomLeft, Position topRight, int width, int height) {
+        int x;
+        int y;
+        int direction = RANDOM.nextInt(4);
+
+        switch (direction) {
+            // To left
+            case 0:
+                x = bottomLeft.getX() - width;
+                y = RANDOM.nextInt(topRight.getY() - bottomLeft.getY()) + bottomLeft.getY();
+                return new Position(x, y);
+
+            // To right
+            case 1:
+                x = topRight.getX() + 1;
+                y = RANDOM.nextInt(topRight.getY() - bottomLeft.getY()) + bottomLeft.getY();
+                return new Position(x, y);
+
+            // To top
+            case 2:
+                x = RANDOM.nextInt(topRight.getX() - bottomLeft.getX()) + bottomLeft.getX();
+                y = topRight.getY() + 1;
+                return new Position(x, y);
+
+            // To bottom
+            case 3:
+                x = RANDOM.nextInt(topRight.getX() - bottomLeft.getX()) + bottomLeft.getX();
+                y = bottomLeft.getY() - height;
+                return new Position(x, y);
+
+            default:
+                x = topRight.getX() + 1;
+                y = RANDOM.nextInt(topRight.getY() - bottomLeft.getY()) + bottomLeft.getY();
+                return new Position(x, y);
+        }
     }
 
     /**
