7425c03fbf474e5ce3fa99094ada835e1999b221
Justin Hsie <justinhsie1001@gmail.com>
2020-08-31 18:07:20 -0700

    Implemented return wallTiles list

--- proj3/byow/WorldGenerator/Room.java
+++ proj3/byow/WorldGenerator/Room.java
@@ -1,10 +1,16 @@
 package byow.WorldGenerator;
 
+import byow.TileEngine.Tileset;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Room {
     private int width;
     private int height;
     private Position bottomLeftCorner;
     private Position topRightCorner;
+    private List<Position> wallTiles;
 
     public Room(int width, int height, Position bottomLeftPosition) {
         this.width = width;
@@ -28,5 +34,22 @@ public class Room {
 
     public Position getTopRightCorner() {
         return topRightCorner;
+    }
+
+    public List<Position> getWallTiles() {
+        wallTiles = new ArrayList<>();
+        int startX = bottomLeftCorner.getX();
+        int startY = bottomLeftCorner.getY();
+        int endX = topRightCorner.getX();
+        int endY = topRightCorner.getY();
+
+        for (int x = startX; x < endX; x += 1) {
+            for (int y = startY; y < endY; y += 1) {
+                if (x == startX || x == endX - 1 || y == startY || y == endY - 1) {
+                    wallTiles.add(new Position(x, y));
+                }
+            }
+        }
+        return wallTiles;
     }
 }
